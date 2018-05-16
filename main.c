3e7d4797906a75fe75c099b4b50ac248fea36b63
vfjpl <vfjpl1@gmail.com>
2018-05-16 22:30:22 +0200

    nowy

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include <gtk/gtk.h>
 #include <mysql/mysql.h>
 
-GtkApplicationWindow *window;
-GtkBuilder *builder;
-GtkTextView *text_field;
-GtkEntry *entry_field;
-
 GtkTextBuffer *text_buffer;
-
+GtkEntry *entry_field;
 MYSQL *connection;
-MYSQL_RES *query_result;
 
 int main (int argc, char *argv[])
 {
@@ -23,8 +17,10 @@ int main (int argc, char *argv[])
     }
 
     //gtk_init
-    gtk_init(&argc, &argv);
+    GtkApplicationWindow *window;
+    GtkBuilder *builder;
 
+    gtk_init(&argc, &argv);
     builder = gtk_builder_new();
     if(gtk_builder_add_from_file(builder, "GuestBook.glade", NULL) == 0)
     {
@@ -34,40 +30,44 @@ int main (int argc, char *argv[])
 
     //get objects defined in .glade file
     window = gtk_builder_get_object(builder, "ApplicationWindow");
-    text_field = gtk_builder_get_object(builder, "TextView_main");
     entry_field = gtk_builder_get_object(builder, "Entry_main");
-    text_buffer = gtk_text_view_get_buffer(text_field);
+    text_buffer = gtk_builder_get_object(builder, "text_buffer");
 
 
     gtk_widget_show(window);
     gtk_main();
 
 
-    mysql_free_result(query_result);
     mysql_close(connection);
     return EXIT_SUCCESS;
 }
 
 void on_button_send()
 {
-    gtk_text_buffer_set_text(text_buffer, gtk_entry_get_text(entry_field), -1);
-    gtk_entry_set_text(entry_field, "");
-    gtk_text_view_set_buffer(text_field, text_buffer);
+    GtkEntryBuffer *temp = gtk_entry_buffer_new(NULL, NULL);
+    gtk_entry_buffer_set_text(temp, "INSERT INTO comments VALUE(NULL,'", -1);//VALUES
+    gtk_entry_buffer_insert_text(temp, gtk_entry_buffer_get_length(temp), gtk_entry_get_text(entry_field), -1);
+    gtk_entry_buffer_insert_text(temp, gtk_entry_buffer_get_length(temp), "',NULL)", -1);
+
+    mysql_query(connection, gtk_entry_buffer_get_text(temp));
 }
 
 void on_button_refresh()
 {
-    mysql_query(connection, "SELECT comment FROM comments ORDER BY id DESC");
-    query_result = mysql_use_result(connection);//mysql_store_result
-
+    mysql_query(connection, "SELECT * FROM comments ORDER BY id DESC");
+    MYSQL_RES *query_result = mysql_use_result(connection);//mysql_store_result
     MYSQL_ROW row;
     if( (row = mysql_fetch_row(query_result)) )
     {
-        gtk_text_buffer_set_text(text_buffer, row[0], -1);
+        gtk_text_buffer_set_text(text_buffer, row[1], -1);
+        gtk_text_buffer_insert_at_cursor(text_buffer, "\n", -1);
+        gtk_text_buffer_insert_at_cursor(text_buffer, row[2], -1);
     }
     while( (row = mysql_fetch_row(query_result)) )
     {
         gtk_text_buffer_insert_at_cursor(text_buffer, "\n\n", -1);
-        gtk_text_buffer_insert_at_cursor(text_buffer, row[0], -1);
+        gtk_text_buffer_insert_at_cursor(text_buffer, row[1], -1);
+        gtk_text_buffer_insert_at_cursor(text_buffer, "\n", -1);
+        gtk_text_buffer_insert_at_cursor(text_buffer, row[2], -1);
     }
 }
